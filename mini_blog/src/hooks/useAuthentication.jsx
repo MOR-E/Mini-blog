8b1097cc730082a771511ffc4f9319353d889c29
MOR-E <pedrohenrique88977944@gmail.com>
2024-03-04 16:43:54 -0300

    ultimo commit 20240304

--- mini_blog/src/hooks/useAuthentication.jsx
+++ mini_blog/src/hooks/useAuthentication.jsx
@@ -81,12 +81,41 @@ export const useAuthentication = () => {
         signOut(auth)
      }
 
+     //LOGIN - SIGN IN
+     const login = async(data) => {
+
+        checkIfIsCancelled()
+
+ 
+        setLoading(true);
+        setError(false);
+     
+        try {
+          await signInWithEmailAndPassword(auth, data.email, data.password);
+          setLoading(false);
+        } catch (error) {
+          let systemErrorMessage;
+          console.log(error)
+     
+          if (error.message.includes("user-not-found")) {
+            systemErrorMessage = "Usuário não encontrado";
+          } else if (error.message.includes("wrong-password")) {
+            systemErrorMessage = "Senha incorreta";
+          } else {
+            systemErrorMessage = "Ocorreu um erro, por favor, tente mais tarde.";
+          }
+     
+          setError(systemErrorMessage);
+          setLoading(false);
+        }
+      };
     return {
         auth,
         createUser,
         error,
         loading,
-        logout
+        logout,
+        login
     }
    
 
